5179cf5f2d9954b7ced3efecadfb4581a3a58f29
jsimpkins <jsimpkins@agi.net>
2019-06-13 17:07:58 -0400

    Add project files.

---
+++ GettingStartedClient/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace GettingStartedClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.lblValue1 = new System.Windows.Forms.Label();
+            this.lblValue2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(64, 12);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 20);
+            this.txtValue1.TabIndex = 0;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(243, 12);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 20);
+            this.txtValue2.TabIndex = 1;
+            // 
+            // lblValue1
+            // 
+            this.lblValue1.AutoSize = true;
+            this.lblValue1.Location = new System.Drawing.Point(15, 19);
+            this.lblValue1.Name = "lblValue1";
+            this.lblValue1.Size = new System.Drawing.Size(43, 13);
+            this.lblValue1.TabIndex = 2;
+            this.lblValue1.Text = "Value 1";
+            // 
+            // lblValue2
+            // 
+            this.lblValue2.AutoSize = true;
+            this.lblValue2.Location = new System.Drawing.Point(194, 19);
+            this.lblValue2.Name = "lblValue2";
+            this.lblValue2.Size = new System.Drawing.Size(43, 13);
+            this.lblValue2.TabIndex = 3;
+            this.lblValue2.Text = "Value 2";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(18, 50);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(102, 50);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtract.TabIndex = 5;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(185, 50);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(268, 50);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 95);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblValue2);
+            this.Controls.Add(this.lblValue1);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Label lblValue1;
+        private System.Windows.Forms.Label lblValue2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+    }
+}
+
---
+++ GettingStartedClient/Form1.cs
@@ -0,0 +1,55 @@
+﻿using GettingStartedClient.Calculatorsvc;
+using System;
+using System.Windows.Forms;
+
+namespace GettingStartedClient
+{
+    public partial class Form1 : Form
+    {
+        CalculatorClient _client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _client = new CalculatorClient();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+            GetValues(out val1, out val2);
+            MessageBox.Show(_client.Add(val1, val2).ToString());
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+            GetValues(out val1, out val2);
+            MessageBox.Show(_client.Subtract(val1, val2).ToString());
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+            GetValues(out val1, out val2);
+            MessageBox.Show(_client.Multiply(val1, val2).ToString());
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+            GetValues(out val1, out val2);
+            MessageBox.Show(_client.Divide(val1, val2).ToString());
+        }
+
+        private void GetValues(out double value1, out double value2)
+        {
+            double.TryParse(txtValue1.Text, out value1);
+            double.TryParse(txtValue2.Text, out value2);
+        }
+    }
+}
